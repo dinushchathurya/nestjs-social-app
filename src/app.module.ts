e43948b137cb20fb4c5174267d2e8cad4fa43d8b
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-29 09:56:36 +0530

    feat: Create feed module and peer to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from  '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FeedModule } from './feed/feed.module';
 import config from 'ormconfig';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(config)
+    TypeOrmModule.forRoot(config),
+    ConfigModule.forRoot({ isGlobal:true }),
+    FeedModule
   ],
   controllers: [AppController],
   providers: [AppService],
